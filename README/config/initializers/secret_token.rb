b2fec8a75a75a8fb64fd87f7434257fee8225809
user <user@test.ru>
2012-10-08 19:10:15 +0400

    rails new README

---
+++ README/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+README::Application.config.secret_token = '0649cfa6432e7a5231e28a70ec383216cf5086d818bc6c30992500e9d158fe90a9a2940155f4d62ce83f636b1af3bcb8d5b8f142d8df70fd8fcf0703f7be4952'
